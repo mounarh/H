37614528e1888243979c606387f3c230aec8001e
S26poste07\IPI <haddou.0014@gmail.com>
2018-11-06 14:22:01 +0100

    commit2

--- h.java
+++ h.java
@@ -2,6 +2,6 @@
 public class h {
 	public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, Worldty");
+        System.out.println("Hello, World holla");
     }
 }
